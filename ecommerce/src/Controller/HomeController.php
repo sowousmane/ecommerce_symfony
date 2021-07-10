f7a90a33c37999821a42292738611e67f76ed1c0
toiwilou <toiwilouhassane@gmail>
2021-07-10 11:51:55 +0200

    add controllers

---
+++ ecommerce/src/Controller/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        try{
+
+            $products = $this->getDoctrine()->getRepository(Product::class)->findAll();
+
+            return $this->render('home/index.html.twig', [
+                'controller_name' => 'HomeController',
+                'products' => $products
+            ]);
+        }
+        catch(\Exception $e){
+            return $this->render('error.html.twig', [
+                'exception' => $e->getMessage(),
+            ]);
+        }
+    }
+}
