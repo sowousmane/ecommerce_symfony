38d8726a67714323a54005eec5395f83448c0c3c
toiwilou <toiwilouhassane@gmail>
2021-07-13 02:55:07 +0200

    make auth

--- ecommerce/src/Controller/HomeController.php
+++ ecommerce/src/Controller/HomeController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Product;
+//use App\Entity\Product;
 
 class HomeController extends AbstractController
 {
@@ -16,11 +16,11 @@ class HomeController extends AbstractController
     {
         try{
 
-            $products = $this->getDoctrine()->getRepository(Product::class)->findAll();
+            //$products = $this->getDoctrine()->getRepository(Product::class)->findAll();
 
             return $this->render('home/index.html.twig', [
                 'controller_name' => 'HomeController',
-                'products' => $products
+                //'products' => $products
             ]);
         }
         catch(\Exception $e){
---
+++ ecommerce/src/Form/CreateAdminFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Admin;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreateAdminFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('Enregistrer', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Admin::class,
+        ]);
+    }
+}
