6ca346a7fcf025b03bfa4fa18f7786619b7cb767
sowousmane <sowousmane4811@gmail.com>
2021-07-20 01:28:45 +0200

    ff

--- ecommerce/src/Controller/HomeController.php
+++ ecommerce/src/Controller/HomeController.php
@@ -30,7 +30,7 @@ class HomeController extends AbstractController
         }
     }
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function home(): Response
     {
