7bda91578d44ac7b5981e2d43d78fba45c24cc8a
sowousmane <sowousmane4811@gmail.com>
2021-07-30 01:51:58 +0200

    login

--- ecommerce/src/Security/LoginAuthenticator.php
+++ ecommerce/src/Security/LoginAuthenticator.php
@@ -49,7 +49,7 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
             return new RedirectResponse($targetPath);
         }
-        
+        return new RedirectResponse('/');
     }
 
     protected function getLoginUrl(Request $request): string
